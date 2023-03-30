82abaec076e4478531649fbd37636f6a8addeb2e
Lucas A. Camacho <111870284+LcsCamacho@users.noreply.github.com>
2023-03-30 10:34:20 +0000

    add middleware config

---
+++ src/middlewares/middleware.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
+dotenv.config();
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    if(req.path === '/login') {
+        console.log('Login route. Skipping auth middleware.')
+        next()
+        return;
+    }
+    if (!token) {
+        console.log('Access denied. No token provided.');
+        return res.status(401).send('Access denied. No token provided.');
+    }
+
+    if (process.env.JWT_PRIVATE_KEY === undefined) return res.status(500).send('Internal server error.');
+
+    jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+        if (err) {
+            res.status(400).json({
+                message: 'Invalid token.',
+                erro:err,
+                decoded:decoded 
+            });
+        } else {
+            next();
+        }
+    });
+}
--- src/routes/veiculoRoute.ts
+++ src/routes/veiculoRoute.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import {inserir, listar, buscarPorPlaca, buscarPorRenavam, deletar, atualizar} from "../controller/veiculo"
 
+import { auth } from "../middlewares/middleware";
+
 export const routerVeiculo = Router();
 
-routerVeiculo.post('/veiculo/inserir', inserir);
+routerVeiculo.post('/veiculo/inserir', auth, inserir);
 routerVeiculo.get('/veiculo/listar', listar);
 routerVeiculo.get('/veiculo/buscarPlaca/:placa', buscarPorPlaca);
 routerVeiculo.get('/veiculo/buscarRenavam/:renavam', buscarPorRenavam);
