f834abd91bc0e61cdc89430faffeabc7c6e9518a
Lucas A. Camacho <111870284+LcsCamacho@users.noreply.github.com>
2023-04-02 01:18:47 -0300

    add try/catch nos endpoints

--- src/controller/abastecimento.ts
+++ src/controller/abastecimento.ts
@@ -8,7 +8,6 @@ export const listar = async (req: Request, res: Response) => {
     if (abastecimento) return res.status(200).json(abastecimento).end();
 
     return res.status(400).send("Erro ao listar abastecimentos").end();
-
 }
 
 export const buscarPorId = async (req: Request, res: Response) => {
@@ -36,7 +35,6 @@ export const buscarPorPlaca = async (req: Request, res: Response) => {
         }
     })
 
-
     if (abastecimento) return res.status(200).json(abastecimento).end();
 
     //se nao encontrar abastecimento com a placa inserida
@@ -46,56 +44,63 @@ export const buscarPorPlaca = async (req: Request, res: Response) => {
 export const inserir = async (req: Request, res: Response) => {
     const { valor, litros, tipo, placa } = req.body;
 
-    const abastecimento = await prisma.abastecimento.create({
-        data: {
-            valor: Number(valor),
-            litros: Number(litros),
-            tipo: tipo,
-            placa: placa
-        }
-    });
-
-    if (abastecimento) return res.status(200).json({abastecimento, message:"Inserido com sucesso"}).end();
-
-    return res.status(400).send("Erro ao inserir abastecimento").end();
+    try {
+        const abastecimento = await prisma.abastecimento.create({
+            data: {
+                valor: Number(valor),
+                litros: Number(litros),
+                tipo: tipo,
+                placa: placa
+            }
+        });
+    
+        if (abastecimento) return res.status(200).json({abastecimento, message:"Inserido com sucesso"}).end();
+        
+    } catch (error) {
+        return res.status(400).send("Erro ao inserir abastecimento").end();
+    }
 }
 
 export const deletar = async (req: Request, res: Response) => {
 
     const { id } = req.params;
 
-    const abastecimento = await prisma.abastecimento.delete({
-        where: {
-            id: Number(id)
-        }
-    })
-
-    if (abastecimento) return res.status(200).json(abastecimento).end();
-
-    //se nao encontrar abastecimento com o id inserido
-    return res.status(400).send("Erro ao deletar abastecimento").end();
-
+    try {
+        const abastecimento = await prisma.abastecimento.delete({
+            where: {
+                id: Number(id)
+            }
+        })
+    
+        if (abastecimento) return res.status(200).json(abastecimento).end();
+        
+    } catch (error) {
+        //se nao encontrar abastecimento com o id inserido
+        return res.status(400).send("Erro ao deletar abastecimento").end();
+    }
 }
 
 export const atualizar = async (req: Request, res: Response) => {
 
     const { id, valor, litros, tipo, placa } = req.body;
 
-    const abastecimento = await prisma.abastecimento.update({
-        where: {
-            id: id
-        },
-        data: {
-            valor: Number(valor),
-            litros: Number(litros),
-            tipo: tipo,
-            placa: placa
-        }
-    })
-
-    if (abastecimento) return res.status(200).json(abastecimento).end();
-
-    //se nao encontrar abastecimento com o id inserido
-    return res.status(400).send("Erro ao atualizar abastecimento").end();
-
+    try {
+        const abastecimento = await prisma.abastecimento.update({
+            where: {
+                id: id
+            },
+            data: {
+                valor: Number(valor),
+                litros: Number(litros),
+                tipo: tipo,
+                placa: placa
+            }
+        })
+    
+        if (abastecimento) return res.status(200).json(abastecimento).end();
+        
+    } catch (error) {
+        //se nao encontrar abastecimento com o id inserido
+        return res.status(400).send("Erro ao atualizar abastecimento").end();
+    }
 }
--- src/controller/usuario.ts
+++ src/controller/usuario.ts
@@ -15,16 +15,20 @@ export const inserir = async (req: Request, res: Response) => {
 
     const { cpf, senha } = req.body;
 
-    const usuario = await prisma.usuario.create({
-        data: {
-            cpf: cpf,
-            senha: senha
-        }
-    })
+    try {
+        const usuario = await prisma.usuario.create({
+            data: {
+                cpf: cpf,
+                senha: senha
+            }
+        })
+    
+        if (usuario) return res.status(200).json({ usuario: usuario, message: "Inserido com sucesso!" }).end();
+    } catch (error) {
+        return res.status(400).json({ error: "Usuário não cadastrado" }).end();
+    }
 
-    if (usuario) return res.status(200).json({ usuario: usuario, message: "Inserido com sucesso!" }).end();
 
-    return res.status(400).json({ error: "Usuário não cadastrado" }).end();
 
 }
 
@@ -32,92 +36,108 @@ export const login = async (req: Request, res: Response) => {
 
     const { cpf, senha } = req.body;
 
-    const usuario = await prisma.usuario.findUnique({
-        where: {
-            cpf: cpf
-        }
-    })
-
-    if (usuario) {
-        //se a senha estiver correta
-        if (usuario.senha === senha) {
-            if (!process.env.JWT_PRIVATE_KEY) {
-                return res.status(500).send('KEY não encontrada.').end();
+    try {
+        const usuario = await prisma.usuario.findUnique({
+            where: {
+                cpf: cpf
             }
-            const data: Usuario = {
-                cpf: usuario.cpf,
-                senha: usuario.senha,
-                token: ''
-            }
-            jwt.sign(data, process.env.JWT_PRIVATE_KEY, { expiresIn: '60m' }, (err, token) => {
-                //se nao der erro, adiciona o token ao objeto data 
-                //e retorna o objeto data
-                if (!err) {
-                    data.token = token
-                    return res.status(200).json(data).end();
-                } else {
-                    return res.status(404).json(err).end();
+        })
+    
+        if (usuario) {
+            //se a senha estiver correta
+            if (usuario.senha === senha) {
+                if (!process.env.JWT_PRIVATE_KEY) {
+                    return res.status(500).send('KEY não encontrada.').end();
                 }
-            });
+                const data: Usuario = {
+                    cpf: usuario.cpf,
+                    senha: usuario.senha,
+                    token: ''
+                }
+                jwt.sign(data, process.env.JWT_PRIVATE_KEY, { expiresIn: '60m' }, (err, token) => {
+                    //se nao der erro, adiciona o token ao objeto data 
+                    //e retorna o objeto data
+                    if (!err) {
+                        data.token = token
+                        return res.status(200).json(data).end();
+                    } else {
+                        return res.status(404).json(err).end();
+                    }
+                });
+            }
+            else return res.status(401).json({ error: "Senha incorreta" }).end();
         }
-        else return res.status(401).json({ error: "Senha incorreta" }).end();
+
+        else return res.status(401).json({ error: "Usuário não encontrado" }).end();
+
+    } catch (error) {
+        return res.status(401).json({ error: "Erro ao fazer login" }).end();
     }
 
     //se nao encontrar nenhum usuario com o cpf informado
-    else return res.status(401).json({ error: "Usuário não encontrado" }).end();
 
 }
 
 export const listar = async (req: Request, res: Response) => {
-    const usuarios = await prisma.usuario.findMany();
-
-    if (usuarios) return res.status(200).json({ usuarios: usuarios }).end();
-    return res.status(401).json({ error: "Nenhum usuário encontrado" }).end();
-
+    try {
+        const usuarios = await prisma.usuario.findMany();
+    
+        if (usuarios) return res.status(200).json({ usuarios: usuarios }).end();
+    } catch (error) {
+        return res.status(401).json({ error: "Nenhum usuário encontrado" }).end();
+    }
 }
 
 export const buscar = async (req: Request, res: Response) => {
     const { cpf } = req.body;
 
-    const usuario = await prisma.usuario.findUnique({
-        where: {
-            cpf: cpf
-        }
-    })
+    try {
+        const usuario = await prisma.usuario.findUnique({
+            where: {
+                cpf: cpf
+            }
+        })
 
-    if (usuario) return res.status(200).json({ usuario: usuario }).end();
-
-    //se nao encontrar nenhum usuario com o cpf informado
-    return res.status(401).json({ error: "Usuário não encontrado" }).end();
+        if (usuario) return res.status(200).json({ usuario: usuario }).end();
+    } catch (error) {
+        //se nao encontrar nenhum usuario com o cpf informado
+        return res.status(401).json({ error: "Usuário não encontrado" }).end();
+    }
 }
+
 
 export const deletar = async (req: Request, res: Response) => {
     const { cpf } = req.body;
 
-    const usuario = await prisma.usuario.delete({
-        where: {
-            cpf: cpf
-        }
-    })
-    if (usuario) return res.status(200).json({ usuario: usuario }).end();
+    try {
+        const usuario = await prisma.usuario.delete({
+            where: {
+                cpf: cpf
+            }
+        })
+        if (usuario) return res.status(200).json({ usuario: usuario }).end();
 
-    //se nao encontrar nenhum usuario com o cpf informado
-    return res.status(401).json({ error: "Usuário não encontrado" }).end();
+    } catch (error) {
+        //se nao encontrar nenhum usuario com o cpf informado
+        return res.status(401).json({ error: "Usuário não encontrado" }).end();
+    }
 }
 
 export const atualizar = async (req: Request, res: Response) => {
     const { cpf, senha } = req.body;
 
-    const usuario = await prisma.usuario.update({
-        where: {
-            cpf: cpf
-        },
-        data: {
-            senha: senha
-        }
-    })
-    if (usuario) return res.status(200).json({ usuario: usuario }).end();
-
-    //se nao encontrar nenhum usuario com o cpf informado
-    return res.status(401).json({ error: "Usuário não encontrado" }).end();
+    try {
+        const usuario = await prisma.usuario.update({
+            where: {
+                cpf: cpf
+            },
+            data: {
+                senha: senha
+            }
+        })
+        if (usuario) return res.status(200).json({ usuario: usuario }).end();
+    } catch (error) {
+        //se nao encontrar nenhum usuario com o cpf informado
+        return res.status(401).json({ error: "Usuário não encontrado" }).end();
+    }
 }
